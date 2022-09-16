4c6b1d817ea461ed64f00983d09050d639e22a5d
Christian Kobb <ck6202@rit.edu>
2022-09-16 10:17:14 -0400

    notifyall implemented as boradcast

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const WebSocketServer = require('ws');
 
 const wss = new WebSocketServer.Server({port: 8191});
 
+const connectedUsers = new Set();
+
+connectedUsers.broadcast = function(data) {
+    for (let user of connectedUsers) {
+        user.send(data);
+    }
+}
+
 wss.on("connection", ws => {
     console.log("new client connected");
+    connectedUsers.add(ws);
 
     ws.on("message", data => {
         let json_data = JSON.parse(data);
@@ -11,12 +20,13 @@ wss.on("connection", ws => {
         console.log(json_data);
         console.log(json_content.name);
         json_content.name = "Foobar";
-        ws.send(JSON.stringify(json_data));
+        connectedUsers.broadcast(JSON.stringify(json_data));
     });
 
 
     ws.on("close", () => {
         console.log('client disconnected');
+        connectedUsers.delete(ws);
     });
 
     ws.onerror = function () {
