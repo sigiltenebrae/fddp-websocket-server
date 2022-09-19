a5d649d97d36684758cea59f1bd7edec0b82f8ed
Christian Kobb <ck6202@rit.edu>
2022-09-19 16:37:54 -0400

    spectators added to players

--- server.js
+++ server.js
@@ -97,29 +97,98 @@ wss.on("connection", ws => {
             else if (msg_content.request === 'player_change') {
                 if (msg_content.game_id && msg_content.player_data) {
                     let game_data = getGame(msg_content.game_id);
+                    if (getPlayer(msg_content.player_data.id, game_data.players) != null) {
+                        for (let i = 0; i < game_data.players.length; i++) {
+                            if (game_data.players[i].id === msg_content.player_data.id){
+                                game_data.players[i] = msg_content.player_data.player;
+                            }
+                        }
+                    }
+                    let out = true;
+                    if (getPlayer(msg_content.player_data.id, game_data.players) == null) {
+                        if (game_data.turn_count > 0) { //the game is in progress
+                            game_data.players.push(msg_content.player_data.player);
+                        }
+                        else {
+                            game_data.players.push(msg_content.player_data.player);
+                        }
+                    }
+                    if (out) {
+                        if (msg_content.player_data.new_deck) {
+                            connectedUsers.broadcast(JSON.stringify({player_data: msg_content.player_data.player}), 4);
+                        }
+                        else {
+                            connectedUsers.broadcast(JSON.stringify({player_data: msg_content.player_data.player}), ws);
+                        }
+                    }
+                }
+            }
+            else if (msg_content.request === 'player_and_temp_change') {
+                if (msg_content.game_id && msg_content.player_data && msg_content.temp_zone) {
+                    console.log('temp zone!!')
+                    console.log(msg_content);
+                    let game_data = getGame(msg_content.game_id);
                     for (let i = 0; i < game_data.players.length; i++) {
                         if (game_data.players[i].id === msg_content.player_data.id){
                             game_data.players[i] = msg_content.player_data.player;
                         }
+                        if (game_data.players[i].id === msg_content.temp_id) {
+                            switch (msg_content.temp_zone_name) {
+                                case 'grave':
+                                    game_data.players[i].grave = msg_content.temp_zone;
+                                    break;
+                                case 'exile':
+                                    game_data.players[i].exile = msg_content.temp_zone;
+                                    break;
+                                case 'temp_zone':
+                                    game_data.players[i].temp_zone = msg_content.temp_zone;
+                                    break;
+                                case 'hand':
+                                    game_data.players[i].hand = msg_content.temp_zone;
+                                    break;
+                                case 'deck':
+                                    game_data.players[i].deck.cards = msg_content.temp_zone;
+                                    break;
+                                case 'commander':
+                                    game_data.players[i].deck.commander = msg_content.temp_zone;
+                                    break;
+                            }
+                        }
                     }
-                    if (getPlayer(msg_content.player_data.id, game_data.players) == null) {
-                        game_data.players.push(msg_content.player_data.player);
-                    }
-                    if (msg_content.player_data.new_deck) {
-                        connectedUsers.broadcast(JSON.stringify({player_data: msg_content.player_data.player}), 4);
-                    }
-                    else {
-                        connectedUsers.broadcast(JSON.stringify({player_data: msg_content.player_data.player}), ws);
-                    }
+                    connectedUsers.broadcast(JSON.stringify({player_temp_data: msg_content.player_data.player, temp_id: msg_content.temp_id, temp_zone: msg_content.temp_zone, temp_zone_name: msg_content.temp_zone_name}), ws);
                 }
             }
             else if (msg_content.request === 'end_turn') {
                 let game_data = getGame(msg_content.game_id);
-                game_data.turn_count ++;
-                game_data.current_turn ++;
-                if (game_data.current_turn > game_data.players.length - 1) {
-                    game_data.current_turn = 0;
+                let previous_turn = JSON.parse(JSON.stringify(game_data.current_turn));
+                while (true) {
+                    game_data.current_turn ++;
+                    console.log(game_data.current_turn);
+                    console.log(previous_turn);
+                    if (game_data.current_turn > game_data.players.length - 1) {
+                        game_data.current_turn = 0;
+                    }
+                    if (game_data.current_turn === previous_turn) { //everyone has scooped, why are you ending the turn
+                        console.log('bad');
+                        break;
+                    }
+                    let good = false;
+                    for (let player of game_data.players) {
+                        if (player.turn === game_data.current_turn) {
+                            if (player.scooped) {
+                                continue;
+                            }
+                            else {
+                                good = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (good) {
+                        break;
+                    }
                 }
+                console.log('turn update');
                 connectedUsers.broadcast(JSON.stringify({turn_data: {turn_count: game_data.turn_count, current_turn: game_data.current_turn}}), 4);
             }
             else if (msg_content.request === 'shake') {
